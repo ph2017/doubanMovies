c6d88e4cba1a41fd0c5509b645f24047a6a4fc3b
phb2016 <billa2013@163.com>
2017-05-15 22:04:26 +0800

    修改豆瓣api请求方式为https

--- app/app.js
+++ app/app.js
@@ -10,8 +10,9 @@ angular.module('moviecat', [
   // 为模块定义一些常量
   .constant('AppConfig', {
     pageSize: 10,
-    listApiAddress: 'http://api.douban.com/v2/movie/',
-    detailApiAddress: 'http://api.douban.com/v2/movie/subject/'
+    listApiAddress: 'https://api.douban.com/v2/movie/',
+    detailApiAddress: 'https://api.douban.com/v2/movie/subject/',
+    defaultCity: '深圳'
   })
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
